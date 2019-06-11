91fe1ac9f01325d0114358e7e30d8dc26443a2f5
Heymann <heymannsharma7161@gmail.com>
2019-06-11 09:20:08 -0400

    new  changes

---
+++ src/thisisexamplerepo/ThisIsExampleRepo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thisisexamplerepo;
+
+/**
+ *
+ * @author heymannsharma
+ */
+public class ThisIsExampleRepo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+       {
+        Beads b1=new Beads();
+        b1.setColor("White");
+        //System.out.println(b1.getColor());
+      
+          Beads b2=new Beads("black");
+        System.out.println(b1.getColor()+ " "+b2.getColor());
+          
+    
+    }
+    
+}
